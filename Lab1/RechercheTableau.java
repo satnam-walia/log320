b4130e37b824203e2c25384bf8fdd007ff36a8f8
satnam <satnamwalia96@gmail.com>
2023-01-16 15:05:02 -0500

    feat(main): initial commit

---
+++ Lab1/RechercheTableau.java
@@ -0,0 +1,20 @@
+package laboratoire1;
+
+public class RechercheTableau{
+
+    // Ne pas changer ces fonctions, elles seront utilisées pour tester votre programme
+    // Elles peuvent cependant servir seulement d'interface et utiliser une méthode "helper" 
+    // avec des paramètres supplémentaires, au besoin. 
+    public int RechercheLineaire(int[] tab, int n, int x){
+        return -1;
+    }
+
+    public int RechercheBinaire(int[] tab, int n, int val){
+        return -1;
+    }
+
+    public int RechercheBinaireModifie(int[] tab, int n, int val){
+        return -1;
+    }
+
+}
