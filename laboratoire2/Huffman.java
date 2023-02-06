f3f32dac92b6ae288b4c834b2d7a0a614160f565
Théo Litzler <theolitzler@gmail.com>
2023-02-06 16:29:19 -0500

    Lecture du fichier, TODO : créer le tab de fréquences

--- laboratoire2/Huffman.java
+++ laboratoire2/Huffman.java
@@ -1,4 +1,8 @@
 package laboratoire2;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
 
 public class Huffman{
 
@@ -10,5 +14,23 @@ public class Huffman{
     public void Decompresser(String nomFichierEntre, String nomFichierSortie){
 
     }
+
+    public 
+    String fileName = "";
+    // utile pour connaître le nombre de caractères lus
+    int numOfReadChar_textMode = 0;
+    BufferedReader br = new BufferedReader(fr);
+
+    try (InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8")) {
+        int singleCharInt;
+        char singleChar;
+        while((singleCharInt = br.read()) != -1) {
+            singleChar = (char) singleCharInt;
+            // singleChar
+            numOfReadChar_textMode++;
+        }
+    }
+
+    public 
 
 }
