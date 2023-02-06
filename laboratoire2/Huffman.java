08414f47ed94bb30cbcf9abc2d56e60079385e6c
Théo Litzler <theolitzler@gmail.com>
2023-02-06 17:30:29 -0500

    Intégration de la hashtable, TODO ajouter les fréquences au fur et à mesure de la lecture

--- laboratoire2/Huffman.java
+++ laboratoire2/Huffman.java
@@ -1,36 +1,41 @@
 package laboratoire2;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.FileNotFoundException;
+// import java.io.FileInputStream;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.*;
 
 public class Huffman{
 
     // Ne pas changer ces fonctions, elles seront utilisées pour tester votre programme
-    public void Compresser(String nomFichierEntre, String nomFichierSortie){
-
+    public void Compresser(String nomFichierEntre, String nomFichierSortie) throws FileNotFoundException,IOException {
+        readFile(nomFichierEntre);
     }
 
     public void Decompresser(String nomFichierEntre, String nomFichierSortie){
 
     }
 
-    public 
-    String fileName = "";
-    // utile pour connaître le nombre de caractères lus
-    int numOfReadChar_textMode = 0;
-    BufferedReader br = new BufferedReader(fr);
 
-    try (InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8")) {
-        int singleCharInt;
-        char singleChar;
-        while((singleCharInt = br.read()) != -1) {
-            singleChar = (char) singleCharInt;
-            // singleChar
-            numOfReadChar_textMode++;
+
+    public void readFile(String fileName) throws FileNotFoundException,IOException {
+        int numOfReadChar_textMode = 0; // pour connaître le nombre de caractères lus
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        Hashtable<String, Integer> freqTable = new Hashtable<>();
+        /* TODO : pour chaque byte => voir s'il existe déjà dans la table
+        ** TODO : si non : l'ajouter
+        ** TODO : si oui; ajouter +1 à sa fréquence
+        */
+
+        try (InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8")) {
+            int singleCharInt;
+            char singleChar;
+            while((singleCharInt = inputStreamReader.read()) != -1) {
+                singleChar = (char) singleCharInt;
+                numOfReadChar_textMode++;
+            }
         }
     }
-
-    public 
 
 }
